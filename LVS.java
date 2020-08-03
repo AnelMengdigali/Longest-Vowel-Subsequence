7a631cf391229a17509532074b825a5906dceb47
AnelMengdigali <54207471+AnelMengdigali@users.noreply.github.com>
2020-08-03 23:29:40 +0600

    Update and rename Ex1.java to LVS.java

--- LVS.java
+++ LVS.java
@@ -67,19 +67,4 @@ class Result{
     }
     
 }
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-        
-        String s = bufferedReader.readLine();
-        
-        int result = Result.longestVowelSubsequence(s);
-        
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-        
-        bufferedReader.close();
-        bufferedWriter.close();
-    }
-}
+
